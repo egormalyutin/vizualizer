ba13c1a0199072f4379f090316d6b82fc35d294e
Egor <malyutinea@inbox.ru>
2019-06-14 23:09:05 +0600

    Remove completed todo's

--- src/postgesql-avg.go
+++ src/postgesql-avg.go
@@ -7,10 +7,6 @@ import (
 )
 
 func (p *PSQL) GetRowsAvg(start, end time.Time, count int) (chan []Column, chan error, chan bool) {
-	// TODO: detect if cache is accessible
-	// TODO: append min and max date
-	// TODO: interpolate when few lines in results
-
 	timeDiff := end.Sub(start)
 
 	cacheLevel := 0
